7810cf9b4409c9226b4b3b46de41d69338d247a5
yasinmnk <77920164+yasinmnk@users.noreply.github.com>
2021-03-09 22:36:55 +0200

    update

---
+++ app.js
@@ -0,0 +1,25 @@
+document.title='DOM-TASK';
+
+const new_div=document.createElement('div');
+var container=document.querySelector('header.container')
+
+new_div.className='yeni-uye';
+new_div.id='yeni-uye1';
+new_div.style='30px';
+new_div.style.margin='25px';
+new_div.style.backgroundColor='red';
+
+
+
+
+console.log(new_div);
+
+
+
+// div.className='container';
+
+// const form=createElement('form');
+
+
+
+// const input=createElement=('input');
